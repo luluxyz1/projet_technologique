2ddd92d01c8b034425d1656192f8c7f874ec957c
Lucas Ferreira <lucas.ferreira@edu.ece.fr>
2024-05-27 11:27:08 +0200

    Projet changé pour Tailwind

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="output.css" rel="stylesheet">
+</head>
+
+<body class="bg-black">
+    <nav class="w-full h-12 ">
+        <div class="h-full w-full bg-red-400">
+            <h1 class="bg-red-500">Test</h1>
+
+        </div>
+    </nav>
+
+</body>
+
+</html>
